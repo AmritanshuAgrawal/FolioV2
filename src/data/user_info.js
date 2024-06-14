2080a9353c5960e41c71c5b8f4af05c026c3e185
ToBeginToPonderOver <121504896+ToBeginToPonderOver@users.noreply.github.com>
2024-06-14 12:10:07 +0530

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,97 @@
+const info = {
+    main: {
+        name: "Amritanshu Agrawal",
+        description: " Passionate about data science and proficient in Python, I excel in project management, team coordination, and developing robust solutions that translate business needs into impactful deliverables. With a keen interest in web development and UI/UX design, I am dedicated to continuous learning and enhancing user experiences through innovative workflows.",
+        role: "Aspiring Data Scientist",
+        photo: "../photo.jpg",
+        email: "amritanshu.agrawal21@st.niituniversity.in",
+    },
+
+    socials: {
+        twitter: "https://x.com/The_MacGuy",
+		github: "https://github.com/ToBeginToPonderOver",
+		linkedin: "https://www.linkedin.com/in/amritanshu-agrawal/",
+		instagram: "https://www.instagram.com/amritanshu_2913/",
+    },
+
+    skills: {
+        languages: {
+            description: "I possess proficient expertise in a diverse range of programming languages, including Python, MySQL, C++, HTML, CSS, and JavaScript.",
+            image: "https://skillicons.dev/icons?i=py,mysql,cpp,html,css,javascript&perline=6&theme="
+        },
+        frameworks: {
+            description: "Experienced in a versatile tech stack, including React, Express.js, Node.js, Flask, Tensorflow, sklearn and more.",
+            image: "https://skillicons.dev/icons?i=react,express,nodejs,flask,tensorflow,sklearn&perline=6&theme="
+        },
+        tools: {
+            description: "I am well-versed in utilizing various tools and platforms to enhance the development process, including Git, Github, Netlify, Vercel, VSCode, PyCharm, and more.",
+            image: "https://skillicons.dev/icons?i=git,github,netlify,vercel,vscode,pycharm&perline=6&theme="
+        },
+    },
+
+    projects: [
+        {
+			title: "Draw & Classify",
+			description: "Draw & Classify is a web application designed to test my machine learning model, which classifies drawn letters, digits, or shapes.",
+			technologies: "https://skillicons.dev/icons?i=react,tailwindcss,tensorflow,0,0&perline=5&theme=",
+			github: "https://github.com/dsbalico/letter-digit-shape-classification",
+            link: "https://letterdigitshape-classification.netlify.app/"
+		},
+
+		{
+			title: "Chatroom",
+			description: "Chatroom is a simple online chatting web application where you can create chat rooms and have a fun conversation with a group of people.",
+			technologies: "https://skillicons.dev/icons?i=react,tailwindcss,firebase,0,0&perline=5&theme=",
+			github: "https://github.com/dsbalico/chatroom",
+            link: "https://thechatroom.netlify.app/"
+		},
+
+		{
+			title: "Sudoku Solver",
+			description: "Sudoku Solver is a web application that can generate or solve sudoku problems using simulated annealing, an optimization algorithm.",
+			technologies: "https://skillicons.dev/icons?i=react,tailwindcss,ts,0,0&perline=5&theme=",
+			github: "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+            link: "https://sudoku-solver-sa.netlify.app/"
+		},
+
+		{
+			title: "Probability Pick",
+			description: "Probability Pick is a PCSO Lotto Number Generator that calculates the likelihood of selecting winning numbers based on historical draw data.",
+			technologies: "https://skillicons.dev/icons?i=react,tailwindcss,flask,0,0&perline=5&theme=",
+			github: "https://github.com/dsbalico/pcso-lotto-number-generator",
+            link: "https://probability-pick.netlify.app/"
+		},
+    ],
+
+    education: [
+        {
+            school: "NIIT University",
+            degree: "B.Tech in Computer Science and Engineering with Specialization in Data Science",
+            year: "2021 - 2025",
+            image: "nu.png"
+        },
+        {
+            school: "Dr. D Y Patil Pushpalata Patil International School",
+            degree: "Senior Secondary Education",
+            year: "2019 - 2021",
+            image: "school.png"
+        },
+    ],
+
+    experience: [
+        { 
+            role: "Data Science Intern",
+            company: "Oasis Infobyte",
+            year: "2024",
+            image: "oasis.png"
+        },
+    ],
+
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at amritanshu.agrawal21@st.niituniversity.in . I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+}
+
+export default info
